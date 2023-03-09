b8456b12aaeaebdd784239aba6bfe99435734877
LETICIA ARAUJO PEREIRA <leticia.1970@aluno.pr.senac.br>
2023-03-09 15:45:11 -0300

    mensagem

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+$menu = 2;
+switch($menu){
+    case 1;
+    case 2;
+        echo "administração";
+    break;
+    case 3;
+        echo "biomedicina";
+    break;
+    case 4;
+        echo "desing grafico";
+    break;
+    case 5; 
+        echo "fisica";
+    break;
+    case 6;
+        echo "jornalismo";
+    break;
+    case 7;
+    case 8;
+    case 9;
+        echo "medicina";
+    break;
+    case 10;
+        echo "relaçoes publicas";
+    break;
+    default:
+        echo "codigo invalido";
+}
+?>
